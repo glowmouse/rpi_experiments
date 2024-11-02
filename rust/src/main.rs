d0a82932c4e34519922d3622f88633c553a717c2
andrew <andrew.brownbill@gmail.com>
2024-11-02 18:01:10 -0400

    Break display into a separate class.

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -14,77 +14,23 @@
 //!
 //! Run on a Blue Pill with `cargo run --bin graphics_i2c_128x32`.
 
-
 #![no_std]
 #![no_main]
 
 use cortex_m::asm::nop;
 use cortex_m_rt::entry;
 use defmt_rtt as _;
-//use embassy_rp::time::Hertz;
-use embedded_graphics::{
-    pixelcolor::BinaryColor,
-    prelude::*,
-    primitives::{Circle, PrimitiveStyleBuilder, Rectangle, Triangle},
-};
 use panic_probe as _;
-use ssd1306::{prelude::*, I2CDisplayInterface, Ssd1306};
-
 
 #[entry]
 fn main() -> ! {
     let p = embassy_rp::init(Default::default());
-    let i2c = embassy_rp::i2c::I2c::new_blocking(
-        p.I2C0,
-        p.PIN_17, // SCLR
+    let mut display = hackernewyears::Display::new(
+        p.I2C0, p.PIN_17, // SCLR
         p.PIN_16, // SDA
-//        Hertz::khz(400),
-        Default::default(),
     );
 
-    let interface = I2CDisplayInterface::new(i2c);
-    let mut display = Ssd1306::new(interface, DisplaySize128x32, DisplayRotation::Rotate0)
-        .into_buffered_graphics_mode();
-    display.init().unwrap();
-
-    let yoffset = 8;
-
-    let style = PrimitiveStyleBuilder::new()
-        .stroke_width(1)
-        .stroke_color(BinaryColor::On)
-        .build();
-
-    // screen outline
-    // default display size is 128x64 if you don't pass a _DisplaySize_
-    // enum to the _Builder_ struct
-    Rectangle::new(Point::new(0, 0), Size::new(127, 31))
-        .into_styled(style)
-        .draw(&mut display)
-        .unwrap();
-
-    // triangle
-    Triangle::new(
-        Point::new(16, 16 + yoffset),
-        Point::new(16 + 16, 16 + yoffset),
-        Point::new(16 + 8, yoffset),
-    )
-    .into_styled(style)
-    .draw(&mut display)
-    .unwrap();
-
-    // square
-    Rectangle::new(Point::new(52, yoffset), Size::new_equal(16))
-        .into_styled(style)
-        .draw(&mut display)
-        .unwrap();
-
-    // circle
-    Circle::new(Point::new(88, yoffset), 16)
-        .into_styled(style)
-        .draw(&mut display)
-        .unwrap();
-
-    display.flush().unwrap();
+    display.update();
 
     loop {
         nop()
