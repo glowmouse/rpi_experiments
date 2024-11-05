20341c487bbf15231a1691b345021ca8013387a2
andrew <andrew.brownbill@gmail.com>
2024-11-05 16:13:57 -0500

    Pulse LED by sine wave.

--- rust/src/sound.rs
+++ rust/src/sound.rs
@@ -11,6 +11,9 @@ use gpio::{Level, Output};
 
 static COUNTER: AtomicU32 = AtomicU32::new(0);
 static PWM: Mutex<CriticalSectionRawMutex, RefCell<Option<Pwm>>> = Mutex::new(RefCell::new(None));
+const BUFFER_SIZE: usize =256;
+static mut BUFFER: [u8; BUFFER_SIZE ] = [0; BUFFER_SIZE ];
+static mut BUFFER_POS: usize = 0;
 
 pub struct Sound<'a> {
     debug_out: Output<'a>,
@@ -22,6 +25,17 @@ impl Sound<'_> {
         debug_pin: embassy_rp::peripherals::PIN_2,
         pwm_slice: embassy_rp::peripherals::PWM_SLICE0 
     ) -> Self {
+
+        for c in 0..BUFFER_SIZE {
+            let a: f32 = ( c as f32 ) / (BUFFER_SIZE as f32 ) * 3.14159265358979323846264338327950288_f32;
+            let s = micromath::F32Ext::sin( a );
+            let ints = (s * 255.0 ) as u8;
+
+            unsafe {
+                BUFFER[c as usize] = ints;
+            }
+        }
+
         let debug_out: Output<'_> = Output::new(debug_pin, Level::High );
         let pwm = embassy_rp::pwm::Pwm::new_output_b(pwm_slice, pin, Default::default());
         PWM.lock(|p| p.borrow_mut().replace(pwm));
@@ -64,6 +78,25 @@ impl Sound<'_> {
 #[interrupt]
 fn PWM_IRQ_WRAP() {
     critical_section::with(|cs| {
+        if (COUNTER.load(Ordering::Relaxed) % 256 ) == 0 {
+            let value:u8;
+            unsafe {
+                BUFFER_POS = ( BUFFER_POS + 1 ) % BUFFER_SIZE;
+                value = BUFFER[ BUFFER_POS ];
+            }
+            // I'm looking to update over 5 seconds
+            // Buffer size is 256, so 51.2 hz
+            // We're triggering every 256 cycles, so 13363 hz
+            // PWM frequency is 62.5Mhz
+            // If I set top to 256, I think I get
+            // 62.5*1024*1024/13363/256, or 19.15
+            let mut config: Config = Config::default();
+            config.divider= FixedU16::from_bits(19*4);
+            config.top = 256;
+            config.compare_b = value as u16;
+            PWM.lock(|p| p.borrow_mut().as_mut().unwrap().set_config(&config));
+        }
+
         PWM.borrow(cs).borrow_mut().as_mut().unwrap().clear_wrapped();
     });
     COUNTER.fetch_add(1, Ordering::Relaxed);
